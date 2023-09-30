cd98ccb2269464f6bb095903228cb9722d714162
Yuwarin05 <thickyuwarin@gmail.com>
2023-09-30 14:28:09 +0700

    Initial commit

---
+++ int101_hw1_66130500073/src/main/java/work04/BankAccount.java
@@ -0,0 +1,45 @@
+package work04;
+
+public class BankAccount {
+    private int number;
+    private Person owner;
+    private double balance;
+
+    //Constructor
+    public BankAccount(int number, Person owner) {
+        this.number = number;
+        this.owner = owner;
+    }
+
+    //Getter
+    public Person getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    public double deposit (double amount) {
+       balance += amount;
+       return balance;
+    }
+    
+    public double withdraw (double amount) {
+        balance -= amount;
+        return balance;
+    }
+    
+    public void transfer (double amount, BankAccount account) {
+        withdraw(amount);
+        account.deposit(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" + "number=" + number + ", owner=" + owner + ", balance=" + balance + '}';
+    }
+    
+    
+    
+}
---
+++ int101_hw1_66130500073/src/main/java/work04/Person.java
@@ -0,0 +1,27 @@
+package work04;
+
+public class Person {
+    private int id;
+
+    //Constructor
+    public Person(int id) {
+        this.id = id;
+    }
+
+    //Getter Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + '}';
+    }
+    
+    
+}
